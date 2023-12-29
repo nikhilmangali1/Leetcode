0127a4dfa9fcac59a07d88d8d6946cdb8a5c2228
Nikhil <82587531+Nikhilnae@users.noreply.github.com>
2023-12-30 00:47:47 +0530

    Time: 1 ms (99.92%), Space: 45.8 MB (9.21%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int value=map.getOrDefault(nums[i],0);
-            map.put(nums[i],value+1);
-        }
-        for(Map.Entry<Integer,Integer> mp:map.entrySet()){
-            if(mp.getValue()==1)
-            {
-                return mp.getKey();
-            }
-        }
-        return -1;
+        int xorRes=0;
+        for(int i:nums) xorRes^=i;
+        return xorRes;
     }
 }
