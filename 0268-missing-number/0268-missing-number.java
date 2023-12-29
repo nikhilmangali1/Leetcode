0d001d3eae4bfc6157c9e8e4a9f1e330c4aa2f7a
Nikhil <82587531+Nikhilnae@users.noreply.github.com>
2023-12-29 23:43:32 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (8.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,18 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int mis=0;
-        for(int i=0;i<nums.length+1;i++){
-            int flag=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[j]==i){
-                    flag=1;
-                    break;
-                }
-            }
-            if(flag==0)
-            {
-                mis=i;
-            }
+        int[] hash=new int[nums.length+1];
+        for(int i=0;i<nums.length;i++){
+            hash[nums[i]]=1;
+        }
+        for(int i=1;i<hash.length;i++){
+            if(hash[i]==0) mis=i;
         }
         return mis;
     }
